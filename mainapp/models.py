c2555589c1fde6789d0c2430599d5ca41b6fb5ed
Sergey <poker4grig@yandex.ru>
2021-09-22 14:15:47 +0300

    added 22/09 models.py, admin.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,13 +1,14 @@
-import uuid
-
 from django.db import models
 
 # Create your models here.
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=64)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -16,6 +17,11 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=3, default=0)
     quantity = models.PositiveIntegerField(default=0)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
+
+
 
 
