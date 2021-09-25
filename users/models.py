df98b1f2014b86f6b9433daccde63a23ff5c2dc7
Sergey <poker4grig@yandex.ru>
2021-09-25 19:19:40 +0300

    preparation login 25/09 and use forms

--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='user_images', blank=True)
+
--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
+from users.forms import UserLoginForm
 # Create your views here.
 
 
 def login(request):
+    form = UserLoginForm()
     context = {
         'title': 'Geekshop - Авторизация',
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
