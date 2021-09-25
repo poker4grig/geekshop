60cdbb0ebc64fd34dc1130a2bfce9d8655e0f881
Sergey <poker4grig@yandex.ru>
2021-09-26 00:40:13 +0300

    added login, logout, register 26/09

--- users/urls.py
+++ users/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.urls import path
 
-from .views import login, register
+from .views import login, register, logout
 
 app_name = 'users'
 urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
-
+    path('logout/', logout, name='logout'),
 ]
