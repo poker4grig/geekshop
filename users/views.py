2fbe19de43a5dfd72f90c79d45e21e0c745c8d97
Sergey <poker4grig@yandex.ru>
2021-09-26 21:47:11 +0300

    added showing messages success for templates and added profile templates 26/09

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
+from django.contrib import auth, messages
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
 from django.urls import reverse
 
-
 from users.forms import UserLoginForm, UserRegisterForm
+
+
 # Create your views here.
 
 
@@ -31,6 +32,7 @@ def register(request):
         form = UserRegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Поздравляем, вы успешно зарегистрировались!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegisterForm()
@@ -41,6 +43,14 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 
+def profile(request):
+    context = {
+        'title': 'Geekshop - Профайл'
+    }
+    return render(request, 'users/profile.html', context)
+
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
