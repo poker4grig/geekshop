605cddd338a48ea8690329647cb27ff65ef69b48
Sergey <poker4grig@yandex.ru>
2021-09-28 14:52:38 +0300

    added app baskets 27/09

--- baskets/urls.py
+++ baskets/urls.py
@@ -13,20 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from mainapp.views import index
 
+app_name = 'baskets'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('mainapp.urls', namespace='mainapp')),
-    path('users/', include('users.urls', namespace='users')),
-    path('baskets/', include('baskets.urls', namespace='baskets')),
+#     path('baskets/', login, name='login'),
+#     path('register/', register, name='register'),
+#     path('profile/', profile, name='profile'),
+#     path('logout/', logout, name='logout'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
