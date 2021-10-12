0443c19660086995a22206b0ae02f8862a17b330
Sergey <poker4grig@yandex.ru>
2021-10-12 20:08:54 +0300

    add forms, CBV for admins

--- admins/forms.py
+++ admins/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from users.forms import UserRegisterForm, UserProfileForm
+from mainapp.models import Product, ProductCategory
 from users.models import User
 
 
@@ -27,4 +28,29 @@ class UserAdminProfileForm(UserProfileForm):
         super(UserAdminProfileForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['readonly'] = False
         self.fields['email'].widget.attrs['readonly'] = False
+
+
+class ProductAdminEditForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(ProductAdminEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name == 'image' or field_name == 'category':
+                field.widget.attrs['class'] = 'form-control'
+            else:
+                field.widget.attrs['class'] = 'form-control py-4'
+
+
+class CategoryAdminEditForm(forms.ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(CategoryAdminEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
 
