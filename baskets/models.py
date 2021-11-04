3274a7e3b8861d811bcc0189f54155d441fb391b
Sergey <poker4grig@yandex.ru>
2021-11-04 18:12:13 +0300

    install memcached, use 'with-cache' and @cache_property

--- baskets/models.py
+++ baskets/models.py
@@ -2,20 +2,23 @@ from django.db import models
 
 from mainapp.models import Product
 from users.models import User
+from django.utils.functional import cached_property
+
+
 # Create your models here.
 
 
-class BasketQuerySet(models.QuerySet):
-    def delete(self, *args, **kwargs):
-        for item in self:
-            item.product.quantity += item.quantity
-            item.product.save()
-        super(BasketQuerySet, self).delete()
+# class BasketQuerySet(models.QuerySet):
+#     def delete(self, *args, **kwargs):
+#         for item in self:
+#             item.product.quantity += item.quantity
+#             item.product.save()
+#         super(BasketQuerySet, self).delete()
 
 
 class Basket(models.Model):
-    objects = BasketQuerySet.as_manager()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # objects = BasketQuerySet.as_manager()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
@@ -28,32 +31,36 @@ class Basket(models.Model):
         return self.quantity * self.product.price
 
     def total_summ(self):
-        baskets = Basket.objects.filter(user=self.user)
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.summ() for basket in baskets)
 
     def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user)
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.quantity for basket in baskets)
 
-    def delete(self, using=None, keep_parents=False):
-        self.product.quantity += self.quantity
-        self.save()
-        super(Basket, self).delete()
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
 
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
-        if self.pk:
-            self.product.quantity -= self.quantity - self.get_item(int(self.pk))
-        else:
-            self.product.quantity -= self.quantity
-        self.product.save()
-        super(Basket, self).save()
+    # def delete(self, using=None, keep_parents=False):
+    #     self.product.quantity += self.quantity
+    #     self.save()
+    #     super(Basket, self).delete()
+    #
+    # def save(self, force_insert=False, force_update=False, using=None,
+    #          update_fields=None):
+    #     if self.pk:
+    #         self.product.quantity -= self.quantity - self.get_item(int(self.pk))
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #     self.product.save()
+    #     super(Basket, self).save()
 
     @staticmethod
     def get_item(pk):
         return Basket.objects.get(pk=pk).quantity
-
-
 
     # @staticmethod
     # def total_summ(user):
@@ -64,4 +71,3 @@ class Basket(models.Model):
     # def total_quantity(user):
     #     baskets = Basket.objects.filter(user=user)
     #     return sum(basket.quantity for basket in baskets)
-
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -91,20 +91,20 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'geekshop',
-        'USER': 'postgres',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'geekshop',
+#         'USER': 'postgres',
+#     }
+# }
 
 
 # Password validation
@@ -236,7 +236,16 @@ if DEBUG:
         'template_profiler_panel.panels.template.TemplateProfilerPanel',
     ]
 
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 120
+CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
 
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 
-
+LOW_CACHE = True
