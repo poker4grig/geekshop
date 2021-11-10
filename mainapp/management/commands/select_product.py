3a563fca41221ad5a64fc32dcf8958311f329c57
Sergey <poker4grig@yandex.ru>
2021-11-10 16:49:47 +0300

    add select_product.py

--- mainapp/management/commands/select_product.py
+++ mainapp/management/commands/select_product.py
@@ -5,5 +5,10 @@ from django.db.models import Q
 from admins.views import db_profile_by_type
 
 
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        products = Product.objects.filter(Q(category__name='Обувь') | Q(category__name='Аксессуары'))
+        print(products)
+        db_profile_by_type('learn db', '', connection.queries)
 
 
